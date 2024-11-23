08d98b334fa4f09e2a5a41496233919b81d4ba2e
Eden Harvey <edensharvey111@gmail.com>
2024-11-22 18:28:43 -0700

    init

---
+++ CarServiceManager/CarServiceManager/CarServiceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarServiceManagerApp.swift
+//  CarServiceManager
+//
+//  Created by Eden Harvey on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CarServiceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarServiceManager/CarServiceManagerTests/CarServiceManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarServiceManagerTests.swift
+//  CarServiceManagerTests
+//
+//  Created by Eden Harvey on 11/22/24.
+//
+
+import Testing
+@testable import CarServiceManager
+
+struct CarServiceManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
